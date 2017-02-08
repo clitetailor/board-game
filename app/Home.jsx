cdf755aae98c203bac84a1a5fa46acf12bde3846
Phùng Đức Nhật <ducnhatphung@gmail.com>
2017-02-08 10:58:07 +0700

    feat: home page wire frame

--- app/Home.jsx
+++ app/Home.jsx
@@ -11,9 +11,35 @@ class Home extends Component {
 		return (
 			<div className="Home">
 				<nav className="navbar bg-navbar">
-					
+					<div className="bg-icon"></div>
+
+					<div className="bg-nav-right">
+						<Link to="about">About</Link>
+						<Link to="news">News</Link>
+						<Link to="login">Login</Link>
+					</div>
 				</nav>
 
+				<div>
+					<div className="bg-typography">
+						<img src="" alt="boardgame"/>
+						<h1>
+							Title
+						</h1>
+						<p>
+							Description
+						</p>
+						<div className="bg-login-form">
+							<form action="#">
+								<label htmlFor="account" className="bg-login-label">Account:</label>
+								<input type="text" name="account" className="bg-login-input"/>
+								<label htmlFor="password" className="bg-login-label">Password:</label>
+								<input type="password" name="password" className="bg-login-input"/>
+								<button type="submit">Login</button>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
